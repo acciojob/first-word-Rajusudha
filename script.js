63d662262bf20d2040814ab8babb9bd9c3c7fc7e
Raju Kumar <129931026+Rajusudha@users.noreply.github.com>
2024-02-13 08:51:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function firstWord(s) {
-  // your code here
+    if (s === '' || !s.includes(' ')) {
+        return s;
+    }
+
+    // Find the index of the first space
+    const spaceIndex = s.indexOf(' ');
+
+    // Extract the first word up to the first space
+    const firstWord = s.slice(0, spaceIndex);
+
+    return firstWord;
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
